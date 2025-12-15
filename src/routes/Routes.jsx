0136fc0e7f02629c5ffd36e34d2705888cc6a7e5
Provash-roy <provashray94@gmail.com>
2025-12-15 23:36:32 +0600

    First Commit after setup basic routing

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>,
+  },
+]);
+export default router;
